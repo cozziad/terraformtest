fb6727bffc4c55499fd02854af51dbd0d38a8d1d
Anthony <cozziad@gmail.com>
2019-08-07 12:20:37 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    profile = "default"
+    region = "us-east-1"
+}
+
+terraform {
+  required_version = "~> 0.12.0"
+
+  backend "local" {}
+}
+
+resource "aws_instance" "example" {
+    tags = {
+        Name = "${terraform.workspace}"
+    }
+    ami = "ami-2757f631"
+    instance_type = "t2.micro"
